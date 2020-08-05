7af0121e02cf051f649548f04597eadfe7a0ff4b
Niklas Gustafsson <niklasg@microsoft.com>
2020-08-05 14:40:10 -0700

    Cleaning up the dashboard presentation a bit.

--- Drivers/CurrentWeather/CurrentWeather.groovy
+++ Drivers/CurrentWeather/CurrentWeather.groovy
@@ -26,16 +26,14 @@
  *
  */
 
-def setVersion()
-{
+def setVersion() {
     appName = "Current Temperature"
 	version = "v0.1.0" 
     dwInfo = "${appName}:${version}"
     sendEvent(name: "dwDriverInfo", value: dwInfo, displayed: true)
 }
 
-def updateVersion() 
-{
+def updateVersion()  {
     log.info "In updateVersion"
     setVersion()
 }
@@ -50,7 +48,7 @@ metadata {
 		attribute "currentWeather", "string"
 		attribute "location", "string"
 		
-		attribute "todayTile", "string"
+		attribute "Weather", "string"
         
         attribute "dwDriverInfo", "string"
         command "updateVersion"
@@ -62,28 +60,23 @@ metadata {
 	}
 }
 
-def installed() 
-{
+def installed()  {
 	runEvery1Hour(poll)
 	poll()
 }
 
-def updated() 
-{
+def updated()  {
 	poll()
 }
 
-def uninstalled() 
-{
+def uninstalled()  {
 	unschedule()
 }
 
-def poll() 
-{
+def poll()  {
 	if(logEnable) log.debug "In poll..."
 
-	try 
-	{
+	try {
 
 		// If we don't already know the station ID, we must retrieve it. We should only have to do this once.
 
@@ -164,8 +157,7 @@ def poll()
 		}
 
 
-		if (state.stationIdentifier != null) 
-		{
+		if (state.stationIdentifier != null) {
 			def params3 = [
 				uri: "https://api.weather.gov/stations/${state.stationIdentifier}/observations",
 				headers: ['User-Agent':'Hubitat Weather Device', Acccept: 'application/json']
@@ -212,13 +204,11 @@ def poll()
 			}
 		}
 	}
-	catch (SocketTimeoutException e) 
-	{
+	catch (SocketTimeoutException e) {
 		if(logEnable) log.debug "Connection to nws.gov API timed out."
 		sendEvent(name: "error", value: "Connection timed out while retrieving data from API", displayed: true)
 	}
-	catch (e) 
-	{
+	catch (e) {
 		if(logEnable) log.debug "Could not retrieve weather data: $e"
 		sendEvent(name: "error", value: "Could not retrieve data from API", displayed: true)
 	}
@@ -226,48 +216,28 @@ def poll()
 	tileMap()
 }
 
-def configure() 
-{
+def configure() {
 	poll()
 }
 
 // Round to the nearest tenth. Useful to get more significant digits on the reading
-def roundToTenth(x)
-{
+def roundToTenth(x) {
 	return ((float)Math.round(x*10.0))/10.0
 }
 
-def convertToTempScale(temp)
-{
+def convertToTempScale(temp) {
 	x = temp.value
-	unit = temp.unitCode
+	isCelsius = temp.unitCode == "unit:degC"
 
-	if (location.temperatureScale == "C")
-	{
-		if (unit == "unit:degC")
-		{
-			return x
-		}
-		else
-		{
-			return (x - 32.0) / 1.8
-		}
+	if (location.temperatureScale == "C") {
+		return isCelsius ? x : (x - 32.0) / 1.8
 	}
-	else
-	{
-		if (unit == "unit:degC")
-		{
-			return x * 1.8 + 32.0
-		}
-		else
-		{
-			return x
-		}
+	else {
+		return isCelsius ? x * 1.8 + 32.0 : x
 	}
 }
 
-def tileMap() 
-{
+def tileMap()  {
 	if(logEnable) log.debug "In tileMap..."
 	def roundedTemp = roundToTenth(state.currentTemperature)
 	state.appDataToday = "<table width='100%'>"
@@ -276,5 +246,5 @@ def tileMap()
 	state.appDataToday+= "<tr><td><div style='font-size:.80em;halign=center;'>${state.currentWeather}</div></td></tr>"
 	state.appDataToday+= "<tr><td><div style='font-size:.80em;halign=center;'>${roundedTemp}&#186;${location.temperatureScale}</div></td></tr>"
 	state.appDataToday+= "</table>"
-	sendEvent(name: "todayTile", value: state.appDataToday, displayed: true)
+	sendEvent(name: "Weather", value: state.appDataToday, displayed: true)
 }
