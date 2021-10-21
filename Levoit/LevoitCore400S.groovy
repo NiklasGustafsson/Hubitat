725d0ec63880c2da88b39ce6f45a21ff5ef0de64
Niklas Gustafsson <niklas.gustafsson@outlook.com>
2021-10-21 14:50:00 -0700

    Adding initial support for Levoit Air Purifiers

---
+++ Levoit/LevoitCore400S.groovy
@@ -0,0 +1,397 @@
+metadata {
+    definition(
+        name: "Levoit Core400S Air Purifier",
+        namespace: "NiklasGustafsson",
+        author: "Niklas Gustafsson",
+        description: "Supports controlling the Levoit 400S air purifier",
+        category: "My Apps",
+        iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+        iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+        iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+        documentationLink: "https://github.com/dcmeglio/hubitat-bond/blob/master/README.md")
+        {
+            capability "Switch"
+            capability "FanControl"
+            capability "Actuator"
+
+            attribute "filter", "number";                              // Filter status (0-100%)
+            attribute "mode", "string";                                // Purifier mode 
+
+            attribute "aqi", "number";                                 // AQI (0-500)
+            attribute "aqiDanger", "string";                           // AQI danger level
+            attribute "aqiColor", "string";                            // AQI HTML color
+            
+            attribute "info", "string";                               // HTML
+
+            command "setDisplay", [[name:"Display*", type: "ENUM", description: "Display", constraints: ["on", "off"] ] ]
+            command "setSpeed", [[name:"Speed*", type: "ENUM", description: "Speed", constraints: ["off", "sleep", "auto", "low", "medium", "high"] ] ]
+            command "setMode",  [[name:"Mode*", type: "ENUM", description: "Mode", constraints: ["manual", "sleep", "auto"] ] ]
+            command "toggle"
+        }
+
+    preferences {
+		input("refreshInterval", "number", title: "<font style='font-size:12px; color:#1a77c9'>Refresh Interval</font>", description: "<font style='font-size:12px; font-style: italic'>Poll VeSync status every N seconds</font>", required: true, defaultValue: 30)
+        input("debugOutput", "bool", title: "Enable debug logging?", defaultValue: false, required: false)
+    }
+}
+
+def installed() {
+	logDebug "Installed with settings: ${settings}"
+    updated();
+}
+
+def updated() {
+	logDebug "Updated with settings: ${settings}"
+    state.clear()
+    unschedule()
+	initialize()
+
+    runIn(3, update)
+
+    // Turn off debug log in 30 minutes
+    if (settings?.debugOutput) runIn(1800, logDebugOff);
+}
+
+def uninstalled() {
+	logDebug "Uninstalled app"
+}
+
+def initialize() {
+	logDebug "initializing"
+}
+
+def on() {
+    logDebug "on()"
+	handlePower(true)
+    handleEvent("switch", "on")
+
+	if (state.speed != null) {
+        setSpeed(state.speed)
+	}
+    else {
+        setSpeed("low")
+    }
+
+    if (state.mode != null) {
+        setMode(state.mode)
+    }
+    else {
+        update()
+    }
+}
+
+def off() {
+    logDebug "off()"
+	handlePower(false)
+    handleEvent("switch", "off")
+    handleEvent("speed", "off")
+}
+
+def toggle() {
+    logDebug "toggle()"
+	if (device.currentValue("switch") == "on")
+		off()
+	else
+		on()
+}
+
+def cycleSpeed() {
+    logDebug "cycleSpeed()"
+    def speed = (state.speed == "low") ? "medium" : ( (state.speed == "medium") ? "high" : "low")
+    
+    if (state.switch == "off")
+    {
+        on()
+    }
+    setSpeed(speed)
+}
+
+def setSpeed(speed) {
+    logDebug "setSpeed(${speed})"
+    if (speed == "off") {
+        off()
+    }
+    else if (speed == "auto") {
+        setMode(speed)
+        state.speed = speed
+        handleEvent("speed", speed)
+    }
+    else if (speed == "sleep") {
+        setMode(speed)
+        handleEvent("speed", "on")
+    }
+    else if (state.mode == "manual") {
+        handleSpeed(speed)
+        state.speed = speed
+        handleEvent("speed", speed)
+    }
+}
+
+def setMode(mode) {
+    logDebug "setMode(${mode})"
+    handleMode(mode)
+    state.mode = mode
+	handleEvent("mode", mode)
+    if (mode == "auto") {
+	    handleEvent("speed", "auto")
+    }
+    else if (mode == "sleep")
+    {
+        handleEvent("speed", "on")
+    }
+    else
+    {
+        handleEvent("speed", state.speed)
+    }
+}
+
+def setDisplay(displayOn) {
+    logDebug "setDisplay(${displayOn})"
+    handleDisplayOn(displayOn)
+}
+
+def mapSpeedToInteger(speed) {
+    switch(speed)
+    {
+        case "1":
+        case "low":
+            return 1;
+        case "2":
+        case "medium":
+            return 2;
+        case "3":
+        case "high":
+            return 3;
+        case "4":
+            return 4;
+
+    }
+    return (speed == "low") ? 1 : ( (speed == "medium") ? 2 : 3)
+}
+
+def mapIntegerStringToSpeed(speed) {
+    return (speed == "1") ? "low" : ( (speed == "2") ? "medium" : "high")
+}
+
+def mapIntegerToSpeed(speed) {
+    return (speed == 1) ? "low" : ( (speed == 2) ? "medium" : "high")
+}
+
+def logDebug(msg) {
+    if (settings?.debugOutput) {
+		log.debug msg
+	}
+}
+
+def logError(msg) {
+    log.error msg
+}
+
+void logDebugOff() {
+  //
+  // runIn() callback to disable "Debug" logging after 30 minutes
+  // Cannot be private
+  //
+  if (settings?.debugOutput) device.updateSetting("debugOutput", [type: "bool", value: false]);
+}
+
+def handlePower(on) {
+
+    def result = false
+
+    parent.sendBypassRequest(device, [
+                data: [ enabled: on, id: 0 ],
+                "method": "setSwitch",
+                "source": "APP" ]) { resp ->
+			if (checkHttpResponse("handleOn", resp))
+			{
+                def operation = on ? "ON" : "OFF"
+                logDebug "turned ${operation}()"
+				result = true
+			}
+		}
+    return result
+}
+
+def handleSpeed(speed) {
+
+    def result = false
+
+    parent.sendBypassRequest(device, [
+                data: [ level: mapSpeedToInteger(speed), id: 0, type: "wind" ],
+                "method": "setLevel",
+                "source": "APP"
+            ]) { resp ->
+			if (checkHttpResponse("handleSpeed", resp))
+			{
+                logDebug "Set speed"
+				result = true
+			}
+		}
+    return result
+}
+
+def handleMode(mode) {
+
+    def result = false
+
+    parent.sendBypassRequest(device, [
+                data: [ "mode": mode ],
+                "method": "setPurifierMode",
+                "source": "APP"
+            ]) { resp ->
+			if (checkHttpResponse("handleMode", resp))
+			{
+                logDebug "Set mode"
+				result = true
+			}
+		}
+    return result
+}
+
+def update() {
+
+    logDebug "update()"
+
+    def result = false
+
+    parent.sendBypassRequest(device,  [
+                "method": "getPurifierStatus",
+                "source": "APP"
+            ]) { resp ->
+			if (checkHttpResponse("update", resp))
+			{
+                handleUpdateStatus(resp, null)
+			}
+		}
+    return result
+}
+
+private void handleUpdateStatus(resp, data)
+{
+    logDebug "handleUpdateStatus()"
+
+	try
+	{
+        if (checkHttpResponse("handleUpdateStatus", resp))
+        {
+            def status = resp.data.result
+
+            logDebug status
+
+            state.speed = mapIntegerToSpeed(status.result.level)
+            state.mode = status.result.mode
+
+            handleEvent("switch", status.result.enabled ? "on" : "off")
+            handleEvent("speed",  mapIntegerToSpeed(status.result.level))
+            handleEvent("mode",   status.result.mode)
+            handleEvent("filter", status.result.filter_life)
+
+            updateAQIandFilter(status.result.air_quality_value.toString(),status.result.filter_life)
+        }
+	}
+	catch (e)
+	{
+        logError e.getMessage()
+//		checkHttpResponse("getDevices", e.getMessage())
+	}
+}
+
+private void handleEvent(name, val)
+{
+    logDebug "handleEvent(${name}, ${val})"
+    device.sendEvent(name: name, value: val)
+}
+
+private void updateAQIandFilter(String val, filter) {
+
+    logDebug "updateAQI(${val})"
+
+    //
+    // Conversions based on https://en.wikipedia.org/wiki/Air_quality_index
+    //
+    BigDecimal pm = val.toBigDecimal();
+
+    BigDecimal aqi;
+
+    if      (pm <  12.1) aqi = convertRange(pm,   0.0,  12.0,   0,  50);
+    else if (pm <  35.5) aqi = convertRange(pm,  12.1,  35.4,  51, 100);
+    else if (pm <  55.5) aqi = convertRange(pm,  35.5,  55.4, 101, 150);
+    else if (pm < 150.5) aqi = convertRange(pm,  55.5, 150.4, 151, 200);
+    else if (pm < 250.5) aqi = convertRange(pm, 150.5, 250.4, 201, 300);
+    else if (pm < 350.5) aqi = convertRange(pm, 250.5, 350.4, 301, 400);
+    else                 aqi = convertRange(pm, 350.5, 500.4, 401, 500);
+
+    handleEvent("AQI", aqi);
+
+    String danger;
+    String color;
+
+    if      (aqi <  51) { danger = "Good";                           color = "7e0023"; }
+    else if (aqi < 101) { danger = "Moderate";                       color = "fff300"; }
+    else if (aqi < 151) { danger = "Unhealthy for Sensitive Groups"; color = "f18b00"; }
+    else if (aqi < 201) { danger = "Unhealthy";                      color = "e53210"; }
+    else if (aqi < 301) { danger = "Very Unhealthy";                 color = "b567a4"; }
+    else if (aqi < 401) { danger = "Hazardous";                      color = "7e0023"; }
+    else {                danger = "Hazardous";                      color = "7e0023"; }
+
+    handleEvent("aqiColor", color)
+    handleEvent("aqiDanger", danger)
+
+    def html = "AQI: ${aqi}<br>PM2.5: ${pm} &micro;g/m&sup3;<br>Filter: ${filter}%"
+
+    handleEvent("info", html)
+}
+
+private BigDecimal convertRange(BigDecimal val, BigDecimal inMin, BigDecimal inMax, BigDecimal outMin, BigDecimal outMax, Boolean returnInt = true) {
+  // Let make sure ranges are correct
+  assert (inMin <= inMax);
+  assert (outMin <= outMax);
+
+  // Restrain input value
+  if (val < inMin) val = inMin;
+  else if (val > inMax) val = inMax;
+
+  val = ((val - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  if (returnInt) {
+    // If integer is required we use the Float round because the BigDecimal one is not supported/not working on Hubitat
+    val = val.toFloat().round().toBigDecimal();
+  }
+
+  return (val);
+}
+
+def handleDisplayOn(displayOn) 
+{
+    logDebug "handleDisplayOn()"
+
+    def result = false
+
+    parent.sendBypassRequest(device, [
+                data: [ "state": (displayOn == "on")],
+                "method": "setDisplay",
+                "source": "APP"
+            ]) { resp ->
+			if (checkHttpResponse("handleDisplayOn", resp))
+			{
+                logDebug "Set display"
+				result = true
+			}
+		}
+    return result
+}
+
+def checkHttpResponse(action, resp) {
+	if (resp.status == 200 || resp.status == 201 || resp.status == 204)
+		return true
+	else if (resp.status == 400 || resp.status == 401 || resp.status == 404 || resp.status == 409 || resp.status == 500)
+	{
+		log.error "${action}: ${resp.status} - ${resp.getData()}"
+		return false
+	}
+	else
+	{
+		log.error "${action}: unexpected HTTP response: ${resp.status}"
+		return false
+	}
+}
